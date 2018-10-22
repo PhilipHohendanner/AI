d6da26623c67a0fc181a9e8eab57f57a1acf4f32
Philip Hohendanner <philip.hohendanner@gmx.at>
2018-10-22 12:16:05 +0200

    update

--- assignment1/rau-exercise-A1/src/at/jku/cp/ai/search/algorithms/BFS.java
+++ assignment1/rau-exercise-A1/src/at/jku/cp/ai/search/algorithms/BFS.java
@@ -1,5 +1,7 @@
 package at.jku.cp.ai.search.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 import at.jku.cp.ai.search.Node;
@@ -12,7 +14,22 @@ public class BFS implements Search
 	@Override
 	public Node search(Node start, Predicate<Node> endPredicate)
 	{
-		// TODO, assignment 1
+		List<Node> queue = new ArrayList<>();
+		Node currentNode;
+		queue.add(start);
+		while(!queue.isEmpty()) {
+			currentNode = queue.get(0);
+			// safety measurement against null nodes
+			if(currentNode == null) {
+				return null;
+			}
+			//checks if current node is goal node
+			if(endPredicate.test(currentNode)) {
+				return currentNode;
+			}
+			queue.remove(0);
+			queue.addAll(currentNode.adjacent());
+		}
 		return null;
 	}
 }
