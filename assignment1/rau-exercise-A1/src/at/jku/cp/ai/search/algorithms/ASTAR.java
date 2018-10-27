a0a87c2131012fbb1366486f7806cac3ac57bd0a
crashrip <34278673+crashrip@users.noreply.github.com>
2018-10-27 16:32:42 +0200

    update astar

--- assignment1/rau-exercise-A1/src/at/jku/cp/ai/search/algorithms/ASTAR.java
+++ assignment1/rau-exercise-A1/src/at/jku/cp/ai/search/algorithms/ASTAR.java
@@ -16,6 +16,9 @@ public class ASTAR implements Search
 	@SuppressWarnings("unused")
 	private Function<Node, Double> cost;
 
+	// class to save costs and heuristic costs
+	// need seperate class that is comparable for stablePriorityQueue
+	// cost contain costs of node + costs from parent node without heuristic costs
 	private final class Costs implements Comparable<Costs> {
 		public final double cost;
 		public final double heuristic;
@@ -39,6 +42,7 @@ public class ASTAR implements Search
 	@Override
 	public Node search(Node start, Predicate<Node> endPredicate) {
 
+		// create stack for alreadyVisited nodes and fringe as StablePriorityQueue with costs saved in own class
 		StackWithFastContains<Node> alreadyVisited = new StackWithFastContains<>();
 		StablePriorityQueue<Costs, Node> fringe = new StablePriorityQueue<>();
 		fringe.add(new Pair(new Costs(heuristic.apply(start), cost.apply(start)), start));
@@ -48,11 +52,14 @@ public class ASTAR implements Search
 			p = fringe.poll();
 			Node n = p.s;
 
+			// goal check
 			if (endPredicate.test(n))
 				return n;
 
+			// check if node has already been visited
 			if (!alreadyVisited.contains(n)) {
 				alreadyVisited.push(n);
+				// expand node and add costs from parent node to costs of node + save heuristic costs seperately -> add to fringe
 				for (Node node : n.adjacent()) {
 					Costs costs = new Costs(p.f.cost + cost.apply(node),heuristic.apply(node));
 					fringe.add(new Pair(costs, node));
